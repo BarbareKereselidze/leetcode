b9baabf2121b37284a7dd553594f0336da4ecec4
BarbareKereselidze <barbarekereselidze17@gmail.com>
2024-01-13 21:10:01 +0400

    add solution to 1572 leetcode problem

---
+++ easy/1572.py
@@ -0,0 +1,39 @@
+"""
+1572. Matrix diagonal sum.
+
+Description:
+Given a square matrix mat, return the sum of the matrix diagonals.
+Only include the sum of all the elements on the primary diagonal and all the elements on the secondary diagonal that are not part of the primary diagonal.
+
+Approach:
+The code iterates through each row of the matrix and accumulates the sum by adding the element at position (i, i) (primary diagonal).
+If the element is not part of the primary diagonal (i.e., i is not equal to n - i - 1, where n is the size of the matrix),
+it also adds the element at position (i, n - i - 1) (secondary diagonal).
+
+Complexity:
+Time complexity: O(n)
+Space complexity: O(1)
+"""
+
+from typing import List
+
+
+class Solution:
+    def diagonalSum(self, mat: List[List[int]]) -> int:
+        sum = 0
+        n = len(mat)
+        for i in range(n):
+            sum += mat[i][i]
+            if i == n - i - 1:
+                continue
+            sum += mat[i][n - i - 1]
+        return sum
+
+
+# Example 1
+mat = [[1, 2, 3],
+       [4, 5, 6],
+       [7, 8, 9]]
+
+solution1 = Solution().diagonalSum(mat)
+print(solution1)
